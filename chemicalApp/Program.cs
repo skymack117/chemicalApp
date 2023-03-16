e7043af7524647b48200dc8f2f080291402a6be4
skymack <skymack@LAPTOP197>
2023-03-17 10:26:43 +1300

    Add project files.

---
+++ chemicalApp/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace chemicalApp
+{
+    
+    internal class Program
+    {
+
+        //globel vearabals
+
+        //lists
+
+        static void Main(string[] args)
+        {
+            Random random = new Random();
+
+        }
+    }
+}
